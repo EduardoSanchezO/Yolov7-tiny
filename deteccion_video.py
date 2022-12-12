1f05852914a61feca8fa6ec4c58cdbe95a374885
Eduardo Francisco Sanchez Ocampo <114952709+EduardoSanchezO@users.noreply.github.com>
2022-12-11 22:01:27 -0600

    Add files via upload

--- deteccion_video.py
+++ deteccion_video.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
     parser.add_argument("--model_def", type=str, default="yolov7-tiny.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="yolov7-tiny.pt", help="path to weights file")
+    parser.add_argument("--weights_path", type=str, default="yolov7-tiny.weights", help="path to weights file")
     parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--webcam", type=int, default=1,  help="Is the video processed video? 1 = Yes, 0 == no" )
